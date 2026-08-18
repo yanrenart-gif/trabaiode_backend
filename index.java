241c0a7f3f9a9ed122629cd142e1a5c968795c20
yanrenart-gif <yan.renart@escola.pr.gov.br>
2026-08-18 21:59:11 +0000

    tentando entender cm funciona o scanner

---
+++ index.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SistemaBancario {
+
+    public static void main(String[] args) {
+        // Inicialização do Scanner e variáveis de controle
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0.0;
+        boolean executando = true;
